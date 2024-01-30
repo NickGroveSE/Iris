46b12b9074f08f1ae2e5a2e3fbe0eedc171ff369
Nick Grove <nickgrove.se@gmail.com>
2024-01-30 15:17:06 -0500

    Early Beginning of Go API

--- api/main.go
+++ api/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	app := gin.Default()
+
+	app.GET("/health", func(c *gin.Context) {
+		var Response struct {
+			Health string
+		}
+		Response.Health = "I am healthy!"
+		c.JSON(http.StatusOK, Response)
+	})
+
+	app.Run(":5000")
+
 }
