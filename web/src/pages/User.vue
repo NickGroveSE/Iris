1268d115fca068b1d17067ae93cac32010668acc
Nick Grove <nickgrove.se@gmail.com>
2024-11-18 22:44:23 -0500

    Vite Env Changes

--- web/src/pages/User.vue
+++ web/src/pages/User.vue
@@ -19,7 +19,7 @@ let tracklistState = reactive({
 onMounted(async () => {
   const response = await axios
     .get(
-      (process.env.BACKEND_URL || "https://localhost:5000") + "/music", {
+      (import.meta.env.VITE_BACKEND_URL || "https://localhost:5000") + "/music", {
         params: { timerange: "short_term"},
       }
     )
@@ -37,7 +37,7 @@ const onClick = async (event) => {
 
   const response = await axios
     .get(
-      (process.env.BACKEND_URL || "https://localhost:5000") + "/music", {
+      (import.meta.env.VITE_BACKEND_URL || "https://localhost:5000") + "/music", {
         params: { timerange: event.target.id},
       }
     )
